ed144121c499a2cb2b845fc1a5f0d4f40898acb6
MagupalliMonika <157418122+MagupalliMonika@users.noreply.github.com>
2026-03-03 18:39:10 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 import cv2
 import json
 from pipeline.preprocessing_pipeline import PreprocessingPipeline
+from utils.image_utils import make_json_safe
 
 def main():
-
-    image_path = "input.jpg"
+    print("loading input image")
+    image_path = "/content/drive/MyDrive/img_processing/imgs/t3_sku.jpg"
     image = cv2.imread(image_path)
-
+    
     pipeline = PreprocessingPipeline(output_dir="output")
 
     final_image, results = pipeline.run(image)
-
+    print(results)
     # Save final image
     cv2.imwrite("output/final_output.jpg", final_image)
 
     # Save JSON report
+    safe_results = make_json_safe(results)
+
     with open("output/report.json", "w") as f:
-        json.dump(results, f, indent=4)
+      json.dump(safe_results, f, indent=4)
 
     print("Processing completed.")
 
